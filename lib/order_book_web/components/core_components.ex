ac4cc532bd04d76b110edb71e5c5599594b8bed2
intercaetera <author@intercaetera.com>
2024-03-05 19:11:24 +0100

    add bidding and asking

--- lib/order_book_web/components/core_components.ex
+++ lib/order_book_web/components/core_components.ex
@@ -368,8 +368,8 @@ defmodule OrderBookWeb.CoreComponents do
         id={@id}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
         class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
-          "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
+          "block w-full rounded-lg bg-black text-white focus:ring-0 sm:text-sm sm:leading-6",
+          "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400 py-0",
           @errors == [] && "border-zinc-300 focus:border-zinc-400",
           @errors != [] && "border-rose-400 focus:border-rose-400"
         ]}
@@ -388,7 +388,7 @@ defmodule OrderBookWeb.CoreComponents do
 
   def label(assigns) do
     ~H"""
-    <label for={@for} class="block text-sm font-semibold leading-6 text-zinc-800">
+    <label for={@for} class="block text-sm font-semibold leading-6 text-white">
       <%= render_slot(@inner_block) %>
     </label>
     """
--- lib/order_book_web/live/order_book_live.ex
+++ lib/order_book_web/live/order_book_live.ex
@@ -1,4 +1,5 @@
 defmodule OrderBookWeb.OrderBookLive do
+  import OrderBookWeb.CoreComponents
   use Phoenix.LiveView
 
   import OrderBookWeb.OrderList
@@ -12,6 +13,15 @@ defmodule OrderBookWeb.OrderBookLive do
         <.order_list type="Asks" color="darkgreen" orders={@book.asks} />
         <.order_list type="Bids" color="darkred" orders={@book.bids} />
       </div>
+
+      <form phx-submit="submit" class="flex flex-col items-center">
+        <.input type="number" label="Volume" name="volume" value="" />
+        <.input type="number" label="Price" name="price" value="" />
+        <div class="flex">
+          <.button name="ask" class="m-2">Ask</.button>
+          <.button name="bid" class="m-2">Bid</.button>
+        </div>
+      </form>
     </div>
     """
   end
@@ -27,6 +37,14 @@ defmodule OrderBookWeb.OrderBookLive do
     {:ok, socket}
   end
 
+  def handle_event("submit", unsigned_params, socket) do
+    :ok = unsigned_params
+    |> cast()
+    |> bid_or_ask(socket.assigns.name)
+
+    {:noreply, socket}
+  end
+
   def handle_info(:update, socket) do
     socket =
       socket
@@ -36,4 +54,24 @@ defmodule OrderBookWeb.OrderBookLive do
   end
 
   defp assign_book(socket, name), do: assign(socket, :book, OrderBook.find_or_create_book(name))
+
+  defp cast(%{"ask" => _, "volume" => volume, "price" => price}) do
+    %{ask: true, volume: String.to_integer(volume), price: String.to_integer(price)}
+  end
+
+  defp cast(%{"bid" => _, "volume" => volume, "price" => price}) do
+    %{bid: true, volume: String.to_integer(volume), price: String.to_integer(price)}
+  end
+
+  defp bid_or_ask(%{ask: true, volume: volume, price: price}, name)
+       when volume > 0 and price > 0 do
+    OrderBook.ask(name, volume, price)
+  end
+
+  defp bid_or_ask(%{bid: true, volume: volume, price: price}, name)
+       when volume > 0 and price > 0 do
+    OrderBook.bid(name, volume, price)
+  end
+
+  defp bid_or_ask(_, _), do: nil
 end
